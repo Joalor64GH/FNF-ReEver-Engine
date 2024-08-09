d466441a4cd3b334809967b69ad2b824e354378c
khuonghoanghuy <khuonghoanghuy@gmail.com>
2024-08-09 14:14:17 +0700

    done crash handle

--- TODO.md
+++ TODO.md
@@ -2,9 +2,10 @@
 - Make source code cleaner []
 - Remove old Discord RPC, unuse class [x]
 - Rework old Options Menu []
-- Rework Discord RPC
+- Rework Discord RPC []
+- Rework old Outdated State []
 - Make own Options Data []
 - Make when paused still be using options []
 - Make chart editor more easier []
 - Make animation debug more easier []
-- Add crash handle []
+- Add crash handle [x]
--- source/Main.hx
+++ source/Main.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxGame;
 import flixel.FlxState;
 import openfl.Assets;
@@ -7,6 +8,20 @@ import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
+
+using StringTools;
+
+#if desktop
+import sys.FileSystem;
+import sys.io.File;
+import sys.io.Process;
+#end
+
+import haxe.CallStack;
+import haxe.io.Path;
+import lime.app.Application;
+import openfl.Lib;
+import openfl.events.UncaughtErrorEvent;
 
 class Main extends Sprite
 {
@@ -65,5 +80,62 @@ class Main extends Sprite
 
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, framerate, framerate, skipSplash, startFullscreen));
 		addChild(new FPS(10, 3, 0xFFFFFF));
+
+		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, (e:UncaughtErrorEvent) -> {
+			#if desktop
+			var errMsg:String = "";
+			var path:String;
+			var callStack:Array<StackItem> = CallStack.exceptionStack(true);
+			var dateNow:String = Date.now().toString();
+
+			dateNow = dateNow.replace(" ", "_");
+			dateNow = dateNow.replace(":", "'");
+
+			path = "./crash/" + "game_" + dateNow + ".txt";
+
+			for (stackItem in callStack) {
+				switch (stackItem) {
+					case FilePos(s, file, line, column):
+						errMsg += file + " (line " + line + ")\n";
+					default:
+						Sys.println(stackItem);
+				}
+			}
+
+			errMsg += "\nUncaught Error: "
+				+ e.error
+				+ "\nPlease report this error to the GitHub page: https://github.com/khuonghoanghuy/FNF-ReEver-Engine\n\n> Crash Handler written by: sqirra-rng";
+
+			try {
+				if (!FileSystem.exists("./crash/"))
+					FileSystem.createDirectory("./crash/");
+
+				File.saveContent(path, errMsg + "\n");
+
+				Sys.println(errMsg);
+				Sys.println("Crash dump saved in " + Path.normalize(path));
+			} catch (e:Dynamic) {
+				Sys.println("Error!\nCouldn't save the crash dump because:\n" + e);
+			}
+
+			if (FlxG.sound.music != null)
+				FlxG.sound.music.stop();
+
+			Application.current.window.alert(errMsg, "Error!");
+			Sys.exit(1);
+			#else
+			var errMsg:String = "";
+
+			errMsg += "\nUncaught Error: "
+			+ e.error
+			+ "\nPlease report this error to the GitHub page: https://github.com/khuonghoanghuy/FNF-ReEver-Engine\n\n> Crash Handler written by: sqirra-rng";
+
+			if (FlxG.sound.music != null)
+				FlxG.sound.music.stop();
+			
+			Application.current.window.alert(errMsg, "Error!");
+			openfl.system.System.exit(1);			
+			#end
+		});
 	}
 }
